218f417e898725505ce08be5152ab51c586753f4
M. Sohail <sohail8080@hotmail.com>
2020-06-28 14:13:21 +0500

    Bootstrap Workinig

---
+++ AppTemplateCore/Areas/Bootstrap/Pages/Grid_Extra_Large.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AppTemplateCore.Areas.Bootstrap.Pages
+{
+    public class Grid__Extra_LargeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
